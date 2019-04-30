5fe03fa1f82cd68f49cbed15e3a46df090316d85
carlosariasf <carlosarias@icreativa.com.co>
2019-04-30 19:11:36 +0000

    Fix \n 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def uppercase(str):
     for i in range(0, len(str)):
-        if i + 1 < len(str):
-            endo = ""
-        else:
-            endo = "\n"
+        if str[i] != '\n':
+            if i + 1 < len(str):
+                endo = ""
+            else:
+                endo = "\n"
         if ord(str[i]) > 96 and ord(str[i]) < 123:
-            print("{}".format(chr(ord(str[i]) - 32)), end=endo)
+            print("{}".format(chr(ord(str[i]) - 32)), end = endo)
         else:
-            print("{}".format(str[i]), end=endo)
+            print("{}".format(str[i]), end = endo)
