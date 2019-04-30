aa06d5dd23cde187eeb8c7720e41886bace592af
carlosariasf <carlosarias@icreativa.com.co>
2019-04-30 17:56:03 +0000

    Push 6 print

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-for c in range(99):
-    print ("{:02d}, ".format(c), end="")
-print ("{:d}".format(99))
+for c in range(1, 100):
+    if (c % 10) > (c / 10) and c != 89:
+        print("{:02d}, ".format(c), end="")
+    elif (c % 10) > (c / 10) and c == 89:
+        print("{:02d}".format(c))
