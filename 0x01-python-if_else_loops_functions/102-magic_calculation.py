4a4ab5b98a4f0afcf301b40dfaffca36ed41c65b
carlosariasf <carlosarias@icreativa.com.co>
2019-04-30 22:35:09 +0000

    Push 102

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def magic_calculation(a, b, c):
     if (a < b):
-        return False
+        return c
     elif (c > b):
-        return False
+        return a + b
     else:
         return (a * b) - c
