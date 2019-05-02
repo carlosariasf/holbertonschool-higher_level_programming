72c97de961f316ac30cec6e12820681f0c6186e7
carlosariasf <carlosarias@icreativa.com.co>
2019-05-02 23:25:27 +0000

    Push 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
     from add_0 import add
-    i = 1
+    a = 1
     b = 2
-    print("a + b = {}".format(add(1, 2)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
