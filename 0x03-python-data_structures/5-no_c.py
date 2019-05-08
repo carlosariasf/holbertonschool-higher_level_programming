d435e06bb3e469ff2e95ae5e9c1e977244d5ab50
carlosariasf <733@holbertonschool.com>
2019-05-08 03:20:42 +0000

    Push 5

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    my_string = list(my_string)
+    for i in range(len(my_string)):
+        if "c" == my_string[i] or "C" == my_string[i]:
+            my_string[i] = ""
+    my_string = "".join(my_string)
+    return my_string
