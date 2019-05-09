e7c972fc5718e731cd00e54f67f2c4cab00b88bb
carlosariasf <733@holbertonschool.com>
2019-05-09 18:40:04 +0000

    Fix copy matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    return list(map(lambda x: (x[0] * x[0], x[1] * x[1], x[2] * x[2]), matrix))
+    matrix2 = map(list, matrix)
+    return list(map(lambda x: (x[0]*x[0], x[1]*x[1], x[2]*x[2]), matrix2))
