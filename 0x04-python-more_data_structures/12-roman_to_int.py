8aca35b9a652eba19a9b234e19836dd1518e63f5
carlosariasf <733@holbertonschool.com>
2019-05-10 03:36:17 +0000

    Push 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 def roman_to_int(roman_string):
     romans = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     sumr = 0
-    if roman_string or type(string) == roman_string:
+    if roman_string and isinstance(roman_string, str):
         nstr = roman_string
         tmp = 0
         for i in range(len(nstr)):
