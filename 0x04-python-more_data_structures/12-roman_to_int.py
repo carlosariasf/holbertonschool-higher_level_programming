629916fbb7674189121dba87bff24fdc8d4f5f1d
carlosariasf <733@holbertonschool.com>
2019-05-10 03:33:27 +0000

    Push 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,12 +2,15 @@
 def roman_to_int(roman_string):
     romans = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     sumr = 0
-    nstr = roman_string
-    tmp = 0
-    for i in range(len(nstr)):
-        tmp = romans[nstr[i]]
-        if i+1 < len(nstr) and romans[nstr[i+1]] > tmp:
-            sumr -= tmp
-        else:
-            sumr += tmp
-    return sumr
+    if roman_string or type(string) == roman_string:
+        nstr = roman_string
+        tmp = 0
+        for i in range(len(nstr)):
+            tmp = romans[nstr[i]]
+            if i+1 < len(nstr) and romans[nstr[i+1]] > tmp:
+                sumr -= tmp
+            else:
+                sumr += tmp
+        return sumr
+    else:
+        return 0
