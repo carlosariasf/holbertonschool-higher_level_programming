59d80cd36a8a26f063dd686c9e8c939bf8468dfa
carlosariasf <733@holbertonschool.com>
2019-05-20 21:55:06 +0000

    [FILE] Try fix task 1

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        print("{:d}".format(value))
+        val = int(value)
+        print("{:d}".format(val))
         return True
     except ValueError:
         return False
