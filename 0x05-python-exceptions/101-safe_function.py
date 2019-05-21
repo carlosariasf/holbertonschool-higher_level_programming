0ff718ea70605395e8599dfd9d70d7cddd93ebeb
carlosariasf <733@holbertonschool.com>
2019-05-21 01:44:04 +0000

    [FILE] Push task 101

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -5,8 +5,6 @@ from sys import stderr
 def safe_function(fct, *args):
     try:
         return fct(args[0], args[1])
-    except IndexError:
-        pass
     except Exception as e:
         print("Exception:", e, file=stderr)
         return None
