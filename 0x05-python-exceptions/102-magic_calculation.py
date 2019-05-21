6c5b807aebed0dca3637a3aa46dc1d97ac3b8f7f
carlosariasf <733@holbertonschool.com>
2019-05-21 03:56:12 +0000

    [FILE] Push task 102

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    result = 0
+    for i in range(a, b):
+        try:
+            i > a
+        except 'Too far':
+            result = a ** b / i
+
+
