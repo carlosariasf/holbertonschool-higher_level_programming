e93a46d7a2e4d7a7e6bf2bc4063233db2637ded9
carlosariasf <733@holbertonschool.com>
2019-05-21 01:20:35 +0000

    [FILE] Fix stderr 100

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+from sys import stderr
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
     except Exception as e:
-        print("Exception:", e)
+        print("Exception:", e, file=stderr)
         return False
