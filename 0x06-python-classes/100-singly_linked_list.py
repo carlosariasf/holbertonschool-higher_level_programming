ad67368bdb48febc67dfb2eda1da9f9a80c8dcd8
carlosariasf <733@holbertonschool.com>
2019-05-22 05:14:10 +0000

    [FILE] Push task 100

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,7 +1,23 @@
 #!/usr/bin/python3
 class Node():
+    '''A class of Node
 
+    Attributes:
+        data: data
+        next_node: node
+    '''
     def __init__(self, data, next_node=None):
-
-        self.__data = data
-        self.__next_node = next_node
+        """Inits Node with data and nextnode, only allowed type int > 0."""
+        try:
+            try:
+                if type(data) == int:
+                    self.__data = data
+            except TypeError as e:
+                raise Exception("data must be an integer") from e
+            try:
+                if type(next_node) == Node:
+                    self._data = next_node
+            except TypeError as e:
+                raise Exception("next_node must be a Node object") from e
+        except Exception as err:
+            print(err)
