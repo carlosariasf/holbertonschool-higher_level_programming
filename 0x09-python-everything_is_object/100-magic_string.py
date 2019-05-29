a6b1395e3717f9ca043148c9459633170c0e043f
carlosariasf <733@holbertonschool.com>
2019-05-29 05:22:32 +0000

    [FILE] push 100

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-def magic_string(count=0):
-    return "{}{}".format("Holberton", (", Holberton" * count))
+def magic_string(count=[0]):
+    count[0] += 1
+    return "{}{}".format("Holberton", (", Holberton" * (count[0] - 1)))
