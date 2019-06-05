3ffb17a86ec3f01c64ad039fda023532f2c9efb9
carlosariasf <733@holbertonschool.com>
2019-06-05 04:40:20 +0000

    [FILE] Push task 9

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -10,11 +10,11 @@ class Rectangle(BG):
     def __init__(self, width, height):
         self.integer_validator("width", width)
         self.integer_validator("height", height)
-        self__width = width
-        self__height = height
+        self.__width = width
+        self.__height = height
 
     def area(self):
-        return self.__width * self.__width
+        return self.__width * self.__height
 
     def __str__(self):
         return "[Rectangle] {}/{}".format(self.__width, self.__height)
