7cf17939e3f9aa278372cb3446241dc88382bcd3
carlosariasf <733@holbertonschool.com>
2019-06-05 03:53:34 +0000

    [FILE] Push files

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" """
+class MyList(list):
+    """ """
+    def __init__(self):
+        """ """
+
+    def print_sorted(self):
+        """ """
+        print (sorted(self))
---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" """
+def add_attribute(nclass, nameu, lastnameu):
+    """ """
+
+    setattr(nclass, name, nameu)
+    setattr(nclass, lastname, lastnameu)
+
+    raise TypeError("can't add new attribute")"""
