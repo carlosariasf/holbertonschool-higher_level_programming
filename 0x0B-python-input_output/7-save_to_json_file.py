77a4115076483e244dbb733fe12f524aa8e144e3
carlosariasf <733@holbertonschool.com>
2019-06-06 03:36:23 +0000

    [FILES] push files

---
+++ 0x0B-python-input_output/7-save_to_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" """
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """ """
+    with open(filename, , encoding='utf-8') as f:
+
+    return json.dumps(my_obj)
