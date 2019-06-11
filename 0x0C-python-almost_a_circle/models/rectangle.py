5e9bee9a645f8b516367735bf692f8769ff4842f
carlosariasf <733@holbertonschool.com>
2019-06-11 18:51:08 +0000

    Test comments Base files

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-""" """
+"""Esto es un comentario"""
 
 
 from .base import Base
 
 
 class Rectangle(Base):
-    """ """
+    """Esto es un comentario"""
+
     def __init__(self, width, height, x=0, y=0, id=None):
-        """ """
+        """Esto es un comentario"""
         super().__init__(id)
         self.width = width
         self.height = height
@@ -17,54 +18,54 @@ class Rectangle(Base):
 
     @property
     def width(self):
-        """ """
+        """Esto es un comentario"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """ """
+        """Esto es un comentario"""
         self.valdata("width", value)
         self.__width = value
 
     @property
     def height(self):
-        """ """
+        """Esto es un comentario"""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """ """
+        """Esto es un comentario"""
         self.valdata("height", value)
         self.__height = value
 
     @property
     def x(self):
-        """ """
+        """Esto es un comentario"""
         return self.__x
 
     @x.setter
     def x(self, value):
-        """ """
+        """Esto es un comentario"""
         self.valdata("x", value)
         self.__x = value
 
     @property
     def y(self):
-        """ """
+        """Esto es un comentario"""
         return self.__y
 
     @y.setter
     def y(self, value):
-        """ """
+        """Esto es un comentario"""
         self.valdata("y", value)
         self.__y = value
 
     def area(self):
-        """ """
+        """Esto es un comentario"""
         return self.__width * self.__height
 
     def display(self):
-        """ """
+        """Esto es un comentario"""
         print(("{}").format('\n' * self.__y), end='')
         for i in range(self.__height):
             print("{}".format(' ' * self.__x), end='')
@@ -72,7 +73,7 @@ class Rectangle(Base):
             print('')
 
     def update(self, *args, **kwargs):
-        """ """
+        """Esto es un comentario"""
         if args and type(args) is int:
             la = len(args)
             if la >= 1:
@@ -100,12 +101,12 @@ class Rectangle(Base):
                         self.y = value
 
     def __str__(self):
-        """ """
+        """Esto es un comentario"""
         return "[Rectangle] ({}) {}/{} - {}/{}".format(
                 self.id, self.__x, self.__y, self.__width, self.__height)
 
     def to_dictionary(self):
-        """ """
+        """Esto es un comentario"""
         dic = {
                 'id': self.id,
                 'width': self.width,
@@ -116,7 +117,7 @@ class Rectangle(Base):
         return dic
 
     def valdata(self, name, value):
-        """ """
+        """Esto es un comentario"""
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
             return
--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,30 +1,31 @@
 #!/usr/bin/python3
-""" """
+"""Esto es un comentario"""
 
 
 from .rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """ """
+    """Esto es un comentario"""
+
     def __init__(self, size, x=0, y=0, id=None):
-        """ """
+        """Esto es un comentario"""
         super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
-        """ """
+        """Esto es un comentario"""
         return self.width
 
     @size.setter
     def size(self, value):
-        """ """
+        """Esto es un comentario"""
         self.valdata("width", value)
         self.width = value
         self.height = value
 
     def update(self, *args, **kwargs):
-        """ """
+        """Esto es un comentario"""
         if args:
             la = len(args)
             if la >= 1:
@@ -50,7 +51,7 @@ class Square(Rectangle):
                         self.y = value
 
     def to_dictionary(self):
-        """ """
+        """Esto es un comentario"""
         dic = {
                 'id': self.id,
                 'size': self.width,
@@ -60,6 +61,6 @@ class Square(Rectangle):
         return dic
 
     def __str__(self):
-        """ """
+        """Esto es un comentario"""
         return ("[Square] ({}) {}/{} - {}".format(
             self.id, self.x, self.y, self.width))
