0f154d63905c32527fad9abf1af70221bda5a269
carlosariasf <733@holbertonschool.com>
2019-06-11 18:46:41 +0000

    Test comments base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-""" """
+"""Esto es un comentario"""
 
 
 import json
 
 
 class Base():
-    """ """
+    """Esto es un comentario"""
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """ """
+        """Esto es un comentario"""
         if id is not None:
             self.id = id
         else:
@@ -18,14 +18,14 @@ class Base():
             self.id = Base.__nb_objects
 
     def to_json_string(list_dictionaries):
-        """ """
+        """Esto es un comentario"""
         if list_dictionaries:
             return json.dumps(list_dictionaries)
         else:
             return "[]"
 
     def from_json_string(json_string):
-        """ """
+        """Esto es un comentario"""
         if json_string:
             return json.loads(json_string)
         else:
@@ -33,14 +33,14 @@ class Base():
 
     @classmethod
     def create(cls, **dictionary):
-        """ """
+        """Esto es un comentario"""
         clsd = cls(1, 1)
         clsd.update(**dictionary)
         return clsd
 
     @classmethod
     def load_from_file(cls):
-        """ """
+        """Esto es un comentario"""
         ilist = []
         with open("{}.json".format(
                 cls.__name__), mode="r", encoding="utf-8") as f:
@@ -51,7 +51,7 @@ class Base():
 
     @classmethod
     def save_to_file(cls, list_objs):
-        """ """
+        """Esto es un comentario"""
         dict2 = []
         with open("{}.json".format(
                 cls.__name__), mode="w", encoding="utf-8") as f:
