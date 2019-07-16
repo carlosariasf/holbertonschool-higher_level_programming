10aa4fee4dd4d6e69c523088b75fedb50e9a4dbd
carlosariasf <733@holbertonschool.com>
2019-07-16 21:03:02 +0000

    Push task 8

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,3 @@
 -- Count ocurrencestable in database
 -- Mysql server 5.7
-SELECT * FROM states WHERE name = 'California' ORDER BY name ASC;
+SELECT * FROM states WHERE name = 'California' ORDER BY id ASC;
