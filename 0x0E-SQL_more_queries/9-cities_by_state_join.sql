5e5aa3bdde6b5ed649407df7c8ad9b30663c2fae
carlosariasf <733@holbertonschool.com>
2019-07-16 22:05:02 +0000

    Push 9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,3 @@
 -- Count ocurrencestable in database
 -- Mysql server 5.7
-SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.id = states.id ORDER BY cities.id ASC;
+SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC;
