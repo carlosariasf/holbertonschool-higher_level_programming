d7b08b44549e2d60c00df250a87a79623af137ad
carlosariasf <733@holbertonschool.com>
2019-07-17 02:14:28 +0000

    Push tasks

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,3 @@
+-- Count ocurrencestable in database
+-- Mysql server 5.7
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id WHERE show_id is NULL ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
