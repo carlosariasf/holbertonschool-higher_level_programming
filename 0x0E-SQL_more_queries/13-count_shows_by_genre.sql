2f6cd2fd3b9e64f39eba5753dc75d0e17107cc82
carlosariasf <733@holbertonschool.com>
2019-07-17 02:18:55 +0000

    Push tasks

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,3 +1,3 @@
 -- Count ocurrencestable in database
 -- Mysql server 5.7
-SELECT tv_genres.name AS genre, COUNT(*) AS number_of_shows FROM tv_genres LEFT JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id GROUP BY tv_genres.name ORDER BY number_of_shows DESC;
+SELECT tv_genres.name AS genre, COUNT(*) AS number_of_shows FROM tv_genres INNER JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id GROUP BY tv_genres.name ORDER BY number_of_shows DESC;
