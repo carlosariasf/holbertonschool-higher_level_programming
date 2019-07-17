42013739ddb25d9fb98598f0c342dd9a0e79b2b6
carlosariasf <733@holbertonschool.com>
2019-07-17 02:51:38 +0000

    task 15

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,3 @@
+-- Count ocurrencestable in database
+-- Mysql server 5.7
+SELECT tv_shows.title FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id WHERE tv_genres.name = 'Comedy' ORDER BY tv_shows.title ASC;
