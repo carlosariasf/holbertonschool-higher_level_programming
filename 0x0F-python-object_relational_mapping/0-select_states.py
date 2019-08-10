a430686c04dfe664b560bda477b4676a22adb8e6
carlosariasf <733@holbertonschool.com>
2019-08-10 18:57:49 +0000

    Remove try task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,29 +6,18 @@ import MySQLdb
 import sys
 
 
-def main():
-    '''Main fuction'''
-    if (len(sys.argv) == 4):
-        db = MySQLdb.connect(
-                host="localhost",
-                port=3306,
-                user=sys.argv[1],
-                passwd=sys.argv[2],
-                db=sys.argv[3])
-        cur = db.cursor()
-        try:
-            cur.execute("SELECT * FROM states ORDER BY states.id ASC;")
-            rows = cur.fetchall()
-        except MySQLdb.Error as e:
-            try:
-                print("MySQL Error [%d]: %s" % (e.args[0], e.args[1]))
-            except IndexError:
-                print("MySQL Error: %s" % str(e))
-        for row in rows:
-            print(row)
-        cur.close()
-        db.close()
-
 if __name__ == "__main__":
-    '''Prevent import'''
-    main()
+    '''Main fuction'''
+    db = MySQLdb.connect(
+    host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC;")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
