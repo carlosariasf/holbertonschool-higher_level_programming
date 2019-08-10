7f5492f567adbeaebd69354bbd42108623f59a07
carlosariasf <733@holbertonschool.com>
2019-08-10 20:41:35 +0000

    Push task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-''' Documentation python3 '''
-
-
+''' lists all states from the database hbtn_0e_0_usa '''
 import MySQLdb
 import sys
 
@@ -14,7 +12,7 @@ if __name__ == "__main__":
             passwd=sys.argv[2],
             db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT id, name FROM states ORDER BY id ASC;")
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
     rows = cur.fetchall()
     for row in rows:
         print(row)
