8bf06a2344211996f1f075aa22cd224c2fb24ed3
carlosariasf <733@holbertonschool.com>
2019-08-10 21:24:27 +0000

    Push 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Documentation python3 """
+""" takes in an argument and displays all values in the states """
 import MySQLdb
 import sys
 
@@ -13,21 +13,15 @@ def main():
             passwd=sys.argv[2],
             db=sys.argv[3])
     cur = db.cursor()
-    try:
-        cur.execute("SELECT * FROM states\
-                WHERE name IN ('{}')\
-                ORDER BY states.id ASC;".format(sys.argv[4]))
-        rows = cur.fetchall()
-    except MySQLdb.Error as e:
-        try:
-            print("MySQL Error [%d]: %s" % (e.args[0], e.args[1]))
-        except IndexError:
-            print("MySQL Error: %s" % str(e))
+    cur.execute("SELECT * FROM states\
+            WHERE name IN ('{}')\
+            ORDER BY states.id ASC;".format(sys.argv[4]))
+    rows = cur.fetchall()
     for row in rows:
         print(row)
     cur.close()
     db.close()
 
 if __name__ == "__main__":
-    '''Prevent import'''
+    """ Prevent import """
     main()
