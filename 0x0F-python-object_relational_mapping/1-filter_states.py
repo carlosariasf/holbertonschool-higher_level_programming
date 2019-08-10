da578b5196e7682aaf5d2fb0a4869d78d18610bf
carlosariasf <733@holbertonschool.com>
2019-08-10 21:06:28 +0000

    Push task 1 test

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/pythonE
-""" Documentation python3 """
+""" lists all states with a name starting with N (upper N) """
 import MySQLdb
 import sys
 
@@ -13,15 +13,9 @@ def main():
             passwd=sys.argv[2],
             db=sys.argv[3])
     cur = db.cursor()
-    try:
-        cur.execute("SELECT * FROM states\
-                WHERE name LIKE 'N%' ORDER BY states.id ASC")
-        rows = cur.fetchall()
-    except MySQLdb.Error as e:
-        try:
-            print("MySQL Error [%d]: %s" % (e.args[0], e.args[1]))
-        except IndexError:
-            print("MySQL Error: %s" % str(e))
+    cur.execute("SELECT * FROM states\
+            WHERE name LIKE 'N%' ORDER BY states.id ASC")
+    rows = cur.fetchall()
     for row in rows:
         print(row)
     cur.close()
