7400ae3b6bc4895709db0935144426a7e615f54e
carlosariasf <733@holbertonschool.com>
2019-08-10 23:53:39 +0000

    Push 5

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" takes in an argument and displays all values in the states """
+import MySQLdb
+import sys
+
+
+def main():
+    """ Main Function """
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT cities.name FROM cities\
+            LEFT JOIN states ON cities.state_id = states.id\
+            WHERE states.name LIKE BINARY %s\
+            ORDER BY cities.id ASC;", (sys.argv[4],))
+    rows = cur.fetchall()
+    i = 1
+    for row in rows:
+        if i < len(rows):
+            print(row[0], end=", ")
+            i += 1
+        else:
+            print(row[0])
+    cur.close()
+    db.close()
+
+if __name__ == "__main__":
+    """ Prevent import """
+    main()
