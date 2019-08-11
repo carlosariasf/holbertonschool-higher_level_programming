3b0088b1bb52534308de36d34d0a59b6190f1765
carlosariasf <733@holbertonschool.com>
2019-08-11 16:30:28 +0000

    Push state

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,19 +1,14 @@
 #!/usr/bin/python3
 """ takes in an argument and displays all values in the states """
-from sys import argv
-from model_state import Base, State
-from sqlalchemy import (create_engine)
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
 
-if __name__ == "__main__":
-    """ Prevent import """
-    engine = create_engine('mysql+mysqldb://%s:%s@localhost:3306/%s', (
-        argv[1], argv[2], argv[3],))
-    Base.metadata.create_all(engine)
-'''
+
+Base = declarative_base()
+
 
 class State(Base):
-    ''' State class inherits from Base '''
+    ''' Class State inherits from Base'''
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, unique=True, nullable=False)
+    id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
-'''
