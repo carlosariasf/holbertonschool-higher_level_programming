0623442c11896c298bd7dd7f7ca85f9e574ce9e5
carlosariasf <733@holbertonschool.com>
2019-08-11 18:19:12 +0000

    Push 10

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" takes in an argument and displays all values in the states """
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+def main():
+    """ Main Function """
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    search = "%s", (sys.argv[4],)
+    Session = sessionmaker()
+    Session.configure(bind=engine)
+    session = Session()
+    states = session.query(State)\
+                    .order_by(State.id)\
+                    .filter(State.name.like('%{}%'.format(search[1][0])))\
+                    .first()
+    if states is not None:
+        print("{}".format(states.id))
+    else:
+        print("Not found")
+
+
+if __name__ == "__main__":
+    """ Prevent import """
+    main()
