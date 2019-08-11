d9d237347e1d687ffda26190e36b09b2d76bbee8
carlosariasf <733@holbertonschool.com>
2019-08-11 19:53:45 +0000

    Push 11

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" takes in an argument and displays all values in the states """
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+def main():
+    """ Main Function """
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Session = sessionmaker()
+    Session.configure(bind=engine)
+    session = Session()
+    new = State(name="Louisiana")
+    session.add(new)
+    session.commit()
+    states = session.query(State)\
+                    .order_by(State.id.desc())\
+                    .first()
+    if states is not None:
+        print(states.id)
+    else:
+        print("Not found")
+
+
+if __name__ == "__main__":
+    """ Prevent import """
+    main()
