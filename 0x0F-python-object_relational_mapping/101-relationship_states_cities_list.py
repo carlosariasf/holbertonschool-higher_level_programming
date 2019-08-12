eff34973219cb6a6b12d6ef89bd69154e4282897
carlosariasf <733@holbertonschool.com>
2019-08-12 01:40:17 +0000

    Push 101

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -22,7 +22,7 @@ def main():
     for state in states:
         print("{}: {}".format(state.id, state.name))
         for city in state.cities:
-            print("    {}: {}".format(city.id, city.name))
+            print("\t{}: {}".format(city.id, city.name))
 
 if __name__ == "__main__":
     """ Prevent import """
