bff57ca1de66b381511e8cc7d4ae57545cac8a0a
carlosariasf <733@holbertonschool.com>
2019-08-12 01:50:54 +0000

    Push 101

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -12,17 +12,18 @@ def main():
     """ Main Function """
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    Session = sessionmaker()
-    Session.configure(bind=engine)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
     session = Session()
     states = session.query(State)\
         .join(City, State.cities)\
         .order_by(State.id)\
         .all()
     for state in states:
-        print("{}: {}".format(state.id, state.name))
+        print(state.id, state.name, sep=": ")
         for city in state.cities:
-            print("\t{}: {}".format(city.id, city.name))
+            print("    ", end="")
+            print(city.id, city.name, sep=": ")
 
 if __name__ == "__main__":
     """ Prevent import """
