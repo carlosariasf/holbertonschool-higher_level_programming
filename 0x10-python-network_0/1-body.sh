4f9be8eebe3cef179cb1a35765d22298e67b78cb
carlosariasf <733@holbertonschool.com>
2019-08-20 18:58:43 +0000

    Push 1

---
+++ 0x10-python-network_0/1-body.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
+coderes=$(curl -sL -o /dev/null -w "%{http_code}" -X GET $1)
+if [ $coderes -eq 200 ]; then
+    curl -sL $1
+fi
