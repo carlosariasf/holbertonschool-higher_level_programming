0ec71f1b1c26ea12b210c3435fc7bb8c1af72888
carlosariasf <733@holbertonschool.com>
2019-08-20 18:32:28 +0000

    Push 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 #Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-coderes=$(curl -s -o /dev/null -w "%{http_code}" -X GET $1)
-if [ $coderes -eq 200 ]; then
-    body=$(curl -sb -X GET $1)
-fi
+body=$(curl -s $1)
 len=${#body}
 echo "$len"
