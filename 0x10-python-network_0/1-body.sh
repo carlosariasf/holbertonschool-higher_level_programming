89070ccd3c0c5689af44b073a54498c8747d3faf
carlosariasf <733@holbertonschool.com>
2019-08-20 19:47:21 +0000

    test 1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl -sLi 34.206.234.184:46063/route_1 | tail -1
+curl -sLi $1 | tail -1
