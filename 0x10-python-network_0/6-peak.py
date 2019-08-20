946f5f2ce73ada980eeedd983cfdad7ccfb1f45d
carlosariasf <733@holbertonschool.com>
2019-08-20 23:26:58 +0000

    Push 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,9 +4,7 @@
 
 def find_peak(list_of_integers):
     """find a peak"""
-    tmp = 0
-    for i in range(len(list_of_integers)):
-        if list_of_integers[i] > tmp:
-            tmp = list_of_integers[i]
-    result = None if tmp == 0 else tmp
-    return result
+    if len(list_of_integers) < 1:
+        return None
+    else:
+        return max(list_of_integers)
