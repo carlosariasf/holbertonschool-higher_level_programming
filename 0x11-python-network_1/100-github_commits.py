6ef8cdb071705e4e7631c302714d0f77ad2e8913
carlosariasf <733@holbertonschool.com>
2019-08-26 22:09:56 +0000

    Push 100

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""get commits"""
+import requests
+from sys import argv
+
+
+url = "https://api.github.com/repos/{}/{}/commits".format(argv[2], argv[1])
+r = requests.get(url)
+req = r.json()
+for i in range(10):
+    print("{}: ".format(req[i].get('sha')), end="")
+    commit = req[i].get("commit")
+    auth = commit.get("author")
+    print("{}".format(auth.get("name")))
