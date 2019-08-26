8cf01cacfe62948e0b84e3a19dc6e9ec8317f666
carlosariasf <733@holbertonschool.com>
2019-08-26 18:19:36 +0000

    Task 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -6,6 +6,7 @@ import sys
 
 if __name__ == "__main__":
     """Python script that fetches https://intranet.hbtn.io/status"""
-    r = requests.post('{}'.format(sys.argv[1]),
-            data = {'email': '{}'.format(sys.argv[2])})
+    r = requests.post('{}'.format(
+        sys.argv[1]),
+        data={'email': '{}'.format(sys.argv[2])})
     print(r.text)
