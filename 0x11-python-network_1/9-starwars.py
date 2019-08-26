dafcbb462a296b7d9bf3c85ee5ea90f170163167
carlosariasf <733@holbertonschool.com>
2019-08-26 21:08:26 +0000

    Task 9

--- 0x11-python-network_1/9-starwars.py
+++ 0x11-python-network_1/9-starwars.py
@@ -7,19 +7,10 @@ import sys
 if __name__ == "__main__":
     """Python script that fetches https://intranet.hbtn.io/status"""
     url = "https://swapi.co/api/people/"
-    if len(sys.argv) < 2:
-        q = '?search='
-    else:
-        q = '?search={}'.format(sys.argv[1])
-    r = requests.get(url + q)
-    try:
-        req = r.json()
-        if req == {}:
-            print("No result")
-        else:
-            print("Number of results: {}".format(req.get("count")))
-            for i in range(len(req["results"])):
-                result = req["results"][i]
-                print("{}".format(result.get("name")))
-    except:
-        print("Not a valid JSON")
+    q = {'search': sys.argv[1]}
+    r = requests.get(url, params=q)
+    req = r.json()
+    print("Number of results: {}".format(req.get("count")))
+    for i in range(len(req["results"])):
+        result = req["results"][i]
+        print("{}".format(result.get("name")))
