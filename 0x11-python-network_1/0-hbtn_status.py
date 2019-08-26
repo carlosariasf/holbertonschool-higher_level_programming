9785e0a31ebef1327d4329858a9cb8f19e6bf82d
carlosariasf <733@holbertonschool.com>
2019-08-26 17:07:05 +0000

    Push 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,7 +7,6 @@ if __name__ == "__main__":
     """Python script that fetches https://intranet.hbtn.io/status"""
     with urllib.request.urlopen(
             'https://intranet.hbtn.io/status') as response:
-        print(dir(response))
         html = response.read()
     print("Body response:")
     print("\t- type: {}".format(type(html)))
