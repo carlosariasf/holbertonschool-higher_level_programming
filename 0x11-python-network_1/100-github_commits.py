aac70c7292f10c807f2b9388fac65227c10107e8
carlosariasf <733@holbertonschool.com>
2019-08-27 05:01:18 +0000

    Push 100

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -9,6 +9,6 @@ if __name__ == "__main__":
     url = "https://api.github.com/repos/{}/{}/commits".format(argv[2], argv[1])
     r = requests.get(url)
     req = r.json()
-    for i in range(10):
-        print("{}: {}".format(req[i].get("sha")),
-              req[i].get("commit").get("author").get("name"))
+    for i in req[:10]:
+        print("{}: {}".format(i.get("sha"),
+              i.get("commit").get("author").get("name")))
