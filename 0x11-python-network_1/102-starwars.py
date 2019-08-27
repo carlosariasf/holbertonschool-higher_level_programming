5ecd28f5962f34eb6ead1d7ac393754c2002f2b5
carlosariasf <733@holbertonschool.com>
2019-08-27 04:45:21 +0000

    task 102

---
+++ 0x11-python-network_1/102-starwars.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Python script that fetches from swapi"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    """Python script that fetches form swapi"""
+    url = "https://swapi.co/api/people/"
+    q = {'search': sys.argv[1]}
+    r = requests.get(url, params=q)
+    cn = r.json()
+    print("Number of results: {}".format(cn.get("count")))
+    result = r.json().get("results")
+    i = 0
+    while i < len(result):
+        print(result[i].get("name"))
+        for j in result[i].get("films"):
+            req = requests.get(j)
+            reqj = req.json()
+            print("\t{}".format(reqj.get("title")))
+        i += 1
+        if i == len(result) and cn.get("next") is not None:
+            r = requests.get(cn.get("next"))
+            result = r.json().get("results")
+            cn = r.json()
+            i = 0
