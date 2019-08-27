3ddeb4c0b8c13f80d35883c9f0e9b7a5454b855b
carlosariasf <733@holbertonschool.com>
2019-08-27 04:50:59 +0000

    100

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -8,11 +8,8 @@ if __name__ == "__main__":
     """Get commits from github"""
     url = "https://api.github.com/repos/{}/{}/commits".format(argv[2], argv[1])
     r = requests.get(url)
-    j = 0
-    for i in sorted(r.json(), key=lambda i: i.get.get("commit")
-                    .get("author").get("date"), reverse=True):
+    req = r.json()
+    req = sorted(req, key=sorted, reverse=True)
+    for i in range(10):
         print("{}: ".format(req[i].get("sha")), end="")
         print("{}".format(req[i].get("commit").get("author").get("name")))
-        j += 1
-        if j == 10:
-            break
