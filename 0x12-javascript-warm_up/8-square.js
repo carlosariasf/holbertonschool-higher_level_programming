242ce7c5a11889952a36ecaf9813885db8ed175f
carlosariasf <carlosarias@icreativa.com.co>
2019-09-17 20:55:17 +0000

    [fix] semistandard compliant

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 
 if (isNaN(parseInt(process.argv[2]))) {
-  console.log('Missing number of occurrences');
+  console.log('Missing size');
 } else {
   for (let i = 0; i < parseInt(process.argv[2]); i++) {
     let sq = '';
     for (let j = 0; j < parseInt(process.argv[2]); j++) {
       sq += 'X';
     }
-    console.log(sq)
+    console.log(sq);
   }
 }
