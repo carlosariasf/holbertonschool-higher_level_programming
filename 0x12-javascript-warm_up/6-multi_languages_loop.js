cbdd74b3ccbe8fa32fa1107bcf166b5ba5a573cd
carlosariasf <733@holbertonschool.com>
2019-09-17 19:35:59 +0000

    Fix semistandard complaint

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 
 let loop = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (i = 0; i < loop.length; i++) {
+
+for (let i = 0; i < loop.length; i++) {
   console.log(loop[i]);
 }
