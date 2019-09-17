b1e70fa1ec7e1e1254941ab6b464454c1380fd7f
carlosariasf <carlosarias@icreativa.com.co>
2019-09-17 20:14:53 +0000

    [fix] semistandard compliant

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
-
-let loop = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-
+const loop = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 for (let i = 0; i < loop.length; i++) {
   console.log(loop[i]);
 }
