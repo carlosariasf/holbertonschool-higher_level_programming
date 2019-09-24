e415bf03fbec1cd549bd80dcb5711d54db43cd29
carlosariasf <carlosarias@icreativa.com.co>
2019-09-24 22:38:49 +0000

    Fix broken checkers

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.readFile('./' + process.argv[2], 'utf8', (error, data) => {
+
+fs.readFile(process.argv[2], 'utf8', (error, data) => {
   if (error) {
     console.log(error);
     return;
-  }
+  } else {
   console.log(data);
+  }
 });
