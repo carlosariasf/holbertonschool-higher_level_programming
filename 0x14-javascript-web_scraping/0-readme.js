7b9dc07a6b7dc548eb7b955cee7b7ddccd0fa06f
carlosariasf <carlosarias@icreativa.com.co>
2019-09-24 22:40:00 +0000

    Fix semistandard

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,8 +4,7 @@ const fs = require('fs');
 fs.readFile(process.argv[2], 'utf8', (error, data) => {
   if (error) {
     console.log(error);
-    return;
   } else {
-  console.log(data);
+    console.log(data);
   }
 });
